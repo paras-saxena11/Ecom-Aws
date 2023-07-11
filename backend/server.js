c67428f55cc7338b1e7a76100bb161e9ebccdfa0
Paras Saxena <paras.saxena11@gmail.com>
2023-07-11 20:07:31 +0530

    build_path added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const http = require('http');
-require('dotenv').config();
-const stripe = require('stripe')(process.env.STRIPE_SECRET);
-const PORT = process.env.PORT;
+const http = require("http");
+require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+const PORT = 8080;
 require("./connection");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
@@ -13,12 +13,28 @@ const io = new Server(server, {
   cors: "https://warm-creponne-4c6353.netlify.app/",
   methods: ["GET", "POST", "PATCH", "DELETE"],
 });
+const path = require("path");
+
+const _dirname = path.dirname("");
+const build_path = path.join(_dirname, "../frontend/build");
+
+app.use(express.static(build_path));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html")),
+    function (error) {
+      if (error) {
+        res.status(500).send(error);
+      }
+    };
+});
 
 const User = require("./models/User");
 const userRoutes = require("./routes/userRoutes");
 const imageRoutes = require("./routes/imageRoutes");
 const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
+const exp = require("constants");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -48,4 +64,4 @@ server.listen(PORT, () => {
   console.log("server running at port", 8080);
 });
 
-app.set('socketio', io);
+app.set("socketio", io);
